a187cf22ccff8e11ac0a7adf85550258126425e8
mateusjunges <mateus@junges.dev>
2022-10-05 22:39:08 -0300

    Handle max recipients

--- src/Api/Message/BatchCollection.php
+++ src/Api/Message/BatchCollection.php
@@ -2,17 +2,33 @@
 
 namespace Ixdf\Postmark\Api\Message;
 
+use App\Exceptions\Notification\BatchMailer\TooManyRecipients;
 use Illuminate\Support\Collection;
 
 class BatchCollection extends Collection
 {
+    private const MAX_RECIPIENTS = 500;
+
     /** @var array<int, \Ixdf\Postmark\Api\Message\BatchMessage> $items */
     protected $items = [];
+
+    /**
+     * @throws \App\Exceptions\Notification\BatchMailer\TooManyRecipients
+     */
+    public function push(...$values): self
+    {
+        if (count($this->items) >= self::MAX_RECIPIENTS) {
+            throw new TooManyRecipients();
+        }
+
+        return parent::push($values);
+    }
 
     public function toJson($options = 0): string
     {
         $items = [];
 
+        /** @var \Ixdf\Postmark\Api\Message\BatchMessage $message */
         foreach ($this->toArray() as $message) {
             $items[] = $message->toArray();
         }
