3237dc137d16098850b6d84a526d062730217531
mateusjunges <mateus@junges.dev>
2022-10-07 17:39:54 -0300

    Trait to extract common batch methods

---
+++ src/Concerns/InteractWithBatches.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ixdf\Postmark\Concerns;
+
+trait InteractWithBatches
+{
+    public function count(): int
+    {
+        return count($this->items);
+    }
+
+    public function all(): array
+    {
+        return $this->items;
+    }
+}
