28d933314cbc2d9a80661c0d57490e4a72077181
mateusjunges <mateus@junges.dev>
2022-10-07 17:42:02 -0300

    New message api fake for tests

---
+++ src/Support/Testing/Fakes/MessagesApiFake.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+
+namespace Ixdf\Postmark\Support\Testing\Fakes;
+
+use Closure;
+use Ixdf\Postmark\Contracts\ApiResponse;
+use Ixdf\Postmark\Contracts\MessageApi;
+use Ixdf\Postmark\Models\Message\Batch;
+use Ixdf\Postmark\Models\Message\BatchWithTemplate;
+use Ixdf\Postmark\Models\Message\EmailWithTemplate;
+use Ixdf\Postmark\Models\Message\Message;
+
+final class MessagesApiFake implements MessageApi
+{
+    private Closure $sendSingleMessageCallback;
+    private Closure $sendBatchesCallback;
+    private Closure $sendTemplatesCallback;
+    private Closure $sendBatchTemplatesCallback;
+
+    public function send(Message $message): ApiResponse
+    {
+        call_user_func($this->sendSingleMessageCallback, $message);
+
+        return ApiResponseFake::create([]);
+    }
+
+    public function sendBatch(Batch $batch): ApiResponse
+    {
+        call_user_func($this->sendBatchesCallback, $batch);
+
+        return ApiResponseFake::create([]);
+    }
+
+    public function sendWithTemplate(EmailWithTemplate $emailWithTemplate): ApiResponse
+    {
+        call_user_func($this->sendTemplatesCallback, $emailWithTemplate);
+
+        return ApiResponseFake::create([]);
+    }
+
+    public function sendBatchWithTemplate(BatchWithTemplate $batchWithTemplate): ApiResponse
+    {
+        call_user_func($this->sendBatchTemplatesCallback, $batchWithTemplate);
+
+        return ApiResponseFake::create([]);
+    }
+
+    public function sendSingleMessageUsing(callable $callback): self
+    {
+        $this->sendSingleMessageCallback = $callback;
+
+        return $this;
+    }
+
+    public function sendBatchesUsing(callable $callback): self
+    {
+        $this->sendBatchesCallback = $callback;
+
+        return $this;
+    }
+
+    public function sendTemplatesUsing(callable $callback): self
+    {
+        $this->sendTemplatesCallback = $callback;
+
+        return $this;
+    }
+
+    public function sendBatchTemplatesUsing(callable $callback): self
+    {
+        $this->sendBatchTemplatesCallback = $callback;
+
+        return $this;
+    }
+}
