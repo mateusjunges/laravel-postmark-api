1e31e225eb650d54978350bef5f12f4d56a5501e
mateusjunges <mateus@junges.dev>
2022-10-07 17:41:42 -0300

    New api response fake

---
+++ src/Support/Testing/Fakes/ApiResponseFake.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Ixdf\Postmark\Support\Testing\Fakes;
+
+use Ixdf\Postmark\Contracts\ApiResponse;
+
+final class ApiResponseFake implements ApiResponse
+{
+    private string $message;
+    private int $errorCode;
+
+    public static function create(array $data): ApiResponse
+    {
+        $model = new self();
+        $model->message = 'OK';
+        $model->errorCode = 0;
+
+        return $model;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function getErrorCode(): int
+    {
+        return $this->errorCode;
+    }
+}
