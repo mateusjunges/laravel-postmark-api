39e335a043f575d20b4f44c68d4bb6bcdf6d0662
mateusjunges <mateus@junges.dev>
2022-10-07 17:41:19 -0300

    Refactor to be used with facade

--- src/Postmark.php
+++ src/Postmark.php
@@ -2,21 +2,16 @@
 
 namespace Ixdf\Postmark;
 
-use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
-use GuzzleHttp\RequestOptions;
-use Ixdf\Postmark\Api\Message;
-use Ixdf\Postmark\Api\Template;
+use Illuminate\Support\Facades\App;
 use Ixdf\Postmark\Contracts\Hydrator;
-use Ixdf\Postmark\Hydrator\ModelHydrator;
+use Ixdf\Postmark\Contracts\MessageApi;
+use Ixdf\Postmark\Contracts\PostmarkService;
+use Ixdf\Postmark\Contracts\TemplateApi;
 
-final class Postmark
+final class Postmark implements PostmarkService
 {
     private const AUTH_HEADER = 'X-Postmark-Server-Token';
-
-    public static $VERIFY_SSL = true;
-
-    private string $apiToken;
 
     public function __construct(
         private ClientInterface $client,
@@ -24,36 +19,19 @@ final class Postmark
     ) {
     }
 
-    public static function create(string $apiToken, string $endpoint = 'https://api.postmarkapp.com/', int $timeout = 30): self
+    public function messages(): MessageApi
     {
-        $client = new Client([
-            'base_uri' => $endpoint,
-            RequestOptions::VERIFY => self::$VERIFY_SSL,
-            RequestOptions::TIMEOUT => $timeout,
-            RequestOptions::HEADERS => [
-                self::AUTH_HEADER => $apiToken,
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-            ]
+        return App::make(MessageApi::class, [
+            'client' => $this->client,
+            'hydrator' => $this->hydrator
         ]);
-
-        return (new self($client, new ModelHydrator()))->setApiToken($apiToken);
     }
 
-    public function setApiToken(string $apiToken): self
+    public function templates(): TemplateApi
     {
-        $this->apiToken = $apiToken;
-
-        return $this;
-    }
-
-    public function messages(): Message
-    {
-        return new Message($this->client, $this->hydrator);
-    }
-
-    public function templates(): Template
-    {
-        return new Template($this->client, $this->hydrator);
+        return App::make(TemplateApi::class, [
+            'client' => $this->client,
+            'hydrator' => $this->hydrator
+        ]);
     }
 }
