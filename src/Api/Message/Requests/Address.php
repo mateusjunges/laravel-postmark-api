7a6e10b3e4c49a364c69c74616c13df8e6b287f8
mateusjunges <mateus@junges.dev>
2022-10-20 16:08:56 -0300

    Add new class to store email addresses

---
+++ src/Api/Message/Requests/Address.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace Ixdf\Postmark\Api\Message\Requests;
+
+final class Address
+{
+    public function __construct(
+        public readonly string $emailAddress,
+        public readonly ?string $fullName = null
+    ) {}
+
+    public function fullName(): string
+    {
+        return $this->fullName !== null
+            ? $this->fullName
+            : $this->emailAddress;
+    }
+}
